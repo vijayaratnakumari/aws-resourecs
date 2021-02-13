0b445aa58a61e44a02b13c1753da4caf9d3bddb1
vijayaratnakumari <78976740+vijayaratnakumari@users.noreply.github.com>
2021-02-13 23:28:12 +0530

    Add existing file

---
+++ create-instances.tf
@@ -0,0 +1,11 @@
+resource  aws_instance example {
+	ami = "ami-04b1ddd35fd71475a"
+	instance_type = "t2.micro"
+	iam_instance_profile = "CodeDeployDemo-EC2-Instance-Profile"
+	subnet_id = "subnet-4aa45212"
+	key_name = "MyUSE1KP"
+	associate_public_ip_address = true
+	security_groups = [ "sg-fdae4086" ]
+	user_data	= "userdata.sh"
+}
+#"vpc-860089e2
